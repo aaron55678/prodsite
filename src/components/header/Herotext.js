eba653b61116b3b661e996958ee2284c248dcaed
aaron55678 <aaron55678@googlemail.com>
2022-04-16 14:58:02 +0100

    couple changes

--- src/components/header/Herotext.js
+++ src/components/header/Herotext.js
@@ -11,12 +11,12 @@ const Herotext = () => {
         <div className='Herotext'>
             <h1 className='Herotext_h1'>Let's play<br /># of Golfers</h1>
             <div className='Herotext_buttongroup'>            
-            <ButtonGroup variant="contained" color="primary" size="large" aria-label="outlined primary button group">
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-            <Button>4</Button>
-            </ButtonGroup>
+                <ButtonGroup variant="contained" color="primary" size="large" aria-label="outlined primary button group">
+                    <Button>1</Button>
+                    <Button>2</Button>
+                    <Button>3</Button>
+                    <Button>4</Button>
+                </ButtonGroup>
             </div>
         </div>
     );
